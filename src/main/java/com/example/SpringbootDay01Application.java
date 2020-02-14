d4166080c57ae5dcd58116eac3bf787e97918cce
8732716 <376093717@qq.com>
2020-02-14 09:57:39 +0800

    some

--- src/main/java/com/example/SpringbootDay01Application.java
+++ src/main/java/com/example/SpringbootDay01Application.java
@@ -9,5 +9,7 @@ public class SpringbootDay01Application {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDay01Application.class, args);
     }
+    public void test(){
 
+    }
 }
